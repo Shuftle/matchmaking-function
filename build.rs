18bafde212dd0deb7cebe275d541901c8b0dcf39
Sebastiano Giordano <46520354+Krahos@users.noreply.github.com>
2026-03-27 12:41:19 +0100

    feat: grouping players in sets of 4

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("./open-match2/proto/mmf.proto")?;
+    tonic_prost_build::configure()
+        .build_client(false)
+        .compile_protos(
+            // List of proto files to compile.
+            &["./open-match2/proto/mmf.proto"],
+            // List of directories where to find imports.
+            &["./open-match2/proto"],
+        )?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,112 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, anyhow};
+use std::{collections::HashMap, pin::Pin};
+use tokio_stream::{Stream, StreamExt};
+use tonic::{Code, Request, Response, Status, Streaming, transport::Server};
+use uuid::Uuid;
+
+use crate::mmf::{
+    ChunkedMmfRunRequest, Match, Roster, StreamedMmfResponse, Ticket,
+    match_making_function_service_server::{
+        MatchMakingFunctionService, MatchMakingFunctionServiceServer,
+    },
+};
+
+pub mod mmf {
+    tonic::include_proto!("open_match2");
+}
+
+#[derive(Default)]
+struct Mff;
+
+#[tonic::async_trait]
+impl MatchMakingFunctionService for Mff {
+    type RunStream = Pin<Box<dyn Stream<Item = Result<StreamedMmfResponse, Status>> + Send>>;
+
+    async fn run(
+        &self,
+        request: Request<Streaming<ChunkedMmfRunRequest>>,
+    ) -> Result<Response<Self::RunStream>, Status> {
+        let mut stream = request.into_inner();
+
+        let mut chunks = Vec::new();
+        while let Some(chunked_request) = stream.next().await {
+            chunks.push(chunked_request?);
+        }
+
+        let tickets: Vec<_> = chunks
+            .into_iter()
+            .filter_map(|chunk| chunk.profile)
+            .flat_map(|profile| profile.pools.into_values())
+            .filter_map(|pool| pool.participants)
+            .flat_map(|roster| roster.tickets)
+            .collect();
+
+        let matches = make_matches(tickets)
+            .context("Failed to create matches")
+            .map_err(|e| {
+                println!("{e}");
+                Status::new(
+                    Code::InvalidArgument,
+                    "Failed to create matches with the provided tickets",
+                )
+            })?;
+
+        // Return stream of responses
+        let output = tokio_stream::iter(
+            matches
+                .into_iter()
+                .map(|m| Ok(StreamedMmfResponse { r#match: Some(m) })),
+        );
+
+        Ok(Response::new(Box::pin(output) as Self::RunStream))
+    }
+}
+
+fn make_matches(tickets: Vec<Ticket>) -> anyhow::Result<Vec<Match>> {
+    tickets
+        .chunks_exact(4)
+        .map(|chunk| {
+            let player_ids: Vec<_> = chunk
+                .iter()
+                .filter_map(|c| c.extensions.get("player_id"))
+                .collect();
+
+            if player_ids.len() != 4 {
+                return Err(anyhow!(
+                    "Expected to have 4 player ids to create a match, we got {}",
+                    player_ids.len()
+                ));
+            }
+
+            let mut rosters = HashMap::new();
+
+            rosters.insert(
+                "players".to_owned(),
+                Roster {
+                    name: "players".to_owned(),
+                    assignment: None,
+                    tickets: tickets.clone(),
+                    extensions: HashMap::default(),
+                },
+            );
+
+            Ok(Match {
+                id: Uuid::new_v4().to_string(),
+                rosters,
+                extensions: HashMap::default(),
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = "127.0.0.1:8000".parse()?;
+    let mmf = Mff::default();
+
+    Server::builder()
+        .add_service(MatchMakingFunctionServiceServer::new(mmf))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
