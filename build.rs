9cc7ceb130b8150f12639d5fc3587c0a0031b312
Sebastiano Giordano <46520354+Krahos@users.noreply.github.com>
2026-03-28 12:13:08 +0100

    chore: removing unwrap from build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     tonic_prost_build::configure()
         .build_client(false)
         .file_descriptor_set_path(out_dir.join("open-match2-descriptor.bin"))
